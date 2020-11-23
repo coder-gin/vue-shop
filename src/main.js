362afe8d54ebd6015bfb33b66308f4bee5abc6fb
Gin <215129113@qq.com>
2020-11-23 20:38:40 +0800

    封装axios及api接口

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import api from '@/api/index'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$api = api
 
 new Vue({
   router,
